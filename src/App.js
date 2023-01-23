231f9d33ebedd61228b5ec11acdf5f73b13c84ff
Izie wood <iw121@ic.ac.uk>
2023-01-23 13:04:02 +0000

    refactor .env

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { useState, useEffect } from 'react'
 import axios from 'axios'
+import config from './utils/config'
 
 import SingleResult from './components/SingleResult'
 import SearchBar from './components/SearchBar'
@@ -10,10 +11,12 @@ const App = () => {
 
   useEffect(() => {
     // eslint-disable-next-line no-undef
-    axios.get(process.env.REACT_APP_API).then((res) => {
-      setResults(res.data.results)
-      setFacets(res.data.facets)
-    })
+    axios.get(`${config.url}${config.APIKey}`)
+      .then((res) => {
+        setResults(res.data.results)
+        setFacets(res.data.facets)
+      })
+      .catch(error => console.log(error))
   }, [])
 
   if (results) {
---
+++ src/utils/config.js
@@ -0,0 +1,7 @@
+const url = process.env.REACT_APP_URL
+const APIKey = process.env.REACT_APP_API_KEY
+
+export default {
+  url,
+  APIKey
+}
